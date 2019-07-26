fae876178d8b3011fb4041612845eefd6e10edb8
Mitch Tabian <mitch@tabian.ca>
2019-07-26 08:50:25 -0700

    deferred async await (parallel execution)

--- app/src/main/java/com/codingwithmitch/coroutineexamples/MainActivity.kt
+++ app/src/main/java/com/codingwithmitch/coroutineexamples/MainActivity.kt
@@ -6,6 +6,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
+import kotlin.system.measureTimeMillis
 
 
 class MainActivity : AppCompatActivity() {
@@ -36,24 +37,47 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
     /**
-     * Job1 and Job2 run in parallel as different coroutines
-	 * Also see "Deferred, Async, Await" branch for parallel execution
+     * async() is a blocking call (similar to the job pattern with job.join())
+    *  NOTES:
+    *  1) IF you don't call await(), it does not wait for the result
+    *  2) Calling await() on both these Deffered values will EXECUTE THEM IN PARALLEL. But the RESULTS won't
+    *     be published until the last result is available (in this case that's result2
      */
     private suspend fun fakeApiRequest() {
         coroutineScope {
 
-            val job1 = launch {
-                println("debug: launching job1 in thread: ${Thread.currentThread().name}")
-                val result1 = getResult1FromApi()
-                setTextOnMainThread("Got $result1")
+            val parentJob = launch {
+
+                val executionTime = measureTimeMillis {
+
+                    val result1: Deferred<String> = async {
+                        println("debug: launching job1 in thread: ${Thread.currentThread().name}")
+                        getResult1FromApi()
+                    }
+                    setTextOnMainThread("Got ${result1.await()}")
+
+                    val result2: Deferred<String> = async {
+                        println("debug: launching job2 in thread: ${Thread.currentThread().name}")
+                        getResult2FromApi()
+                    }
+
+                    setTextOnMainThread("Got ${result2.await()}")
+                }
+                println("debug: job1 and job2 are complete. It took ${executionTime} ms")
+
             }
 
-            val job2 = launch {
-                println("debug: launching job2 in thread: ${Thread.currentThread().name}")
-                val result2 = getResult2FromApi()
-                setTextOnMainThread("Got $result2")
+            // Separate job within the same coroutine context that runs independently of parentJob, Job1 and Job2
+            launch {
+                for(delay in arrayOf(1, 2, 3, 4, 5, 6)){
+                    delay(500)
+                    println("debug: is parent job active?: ${parentJob.isActive}")
+                }
             }
+
+
         }
     }
 
