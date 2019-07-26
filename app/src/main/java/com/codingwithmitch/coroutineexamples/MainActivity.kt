206fe06a2a787d3e598081bd0a44e0804f7d46a3
Mitch Tabian <mitch@tabian.ca>
2019-07-25 19:08:51 -0700

    parallel coroutine jobs

--- app/src/main/java/com/codingwithmitch/coroutineexamples/MainActivity.kt
+++ app/src/main/java/com/codingwithmitch/coroutineexamples/MainActivity.kt
@@ -10,7 +10,6 @@ import kotlinx.coroutines.Dispatchers.Main
 
 class MainActivity : AppCompatActivity() {
 
-    private val JOB_TIMEOUT = 1900L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +19,7 @@ class MainActivity : AppCompatActivity() {
             setNewText("Click!")
 
             CoroutineScope(IO).launch {
+                println("debug: CoroutineScope")
                 fakeApiRequest()
             }
         }
@@ -36,64 +36,25 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * Job1 and Job2 run in parallel as different coroutines
+     */
     private suspend fun fakeApiRequest() {
         coroutineScope {
 
-            val job = launch {
-                try{
-                    val result1 = getResult1FromApi() // wait until job is done
-
-                    if ( result1.equals("Result #1")) {
-
-                        setTextOnMainThread("Got $result1")
-
-                        val result2 = getResult2FromApi() // wait until job is done
-
-                        if (result2.equals("Result #2")) {
-                            setTextOnMainThread("Got $result2")
-                        } else {
-                            setTextOnMainThread("Couldn't get Result #2")
-                        }
-                    } else {
-                        setTextOnMainThread("Couldn't get Result #1")
-                    }
-                }catch (e: CancellationException){
-                    println("debug: CancellationException: ${e.message}")
-                }
-                finally {
-                    println("debug: Finishing job. Cleaning up resources...")
-                }
-
+            val job1 = launch {
+                println("debug: launching job1 in thread: ${Thread.currentThread().name}")
+                val result1 = getResult1FromApi()
+                setTextOnMainThread("Got $result1")
             }
 
-            isJobRunning(job.isActive)
-
-            delay(JOB_TIMEOUT) // wait to see if job completes in this time
-
-            // Cancel Option 1
-            job.cancel(CancellationException("Job took longer than $JOB_TIMEOUT")) // cancel if delay time elapses and job has not completed
-            job.join() // wait for the cancellation to happen
-
-//             Cancel Option 2
-//            job.cancelAndJoin()
-
-            delay(1000)
-
-            isJobRunning(job.isActive)
-
-
+            val job2 = launch {
+                println("debug: launching job2 in thread: ${Thread.currentThread().name}")
+                val result2 = getResult2FromApi()
+                setTextOnMainThread("Got $result2")
+            }
         }
     }
-
-    private fun isJobRunning(isActive: Boolean){
-        if(isActive){
-            println("debug: Job is active")
-        }
-        else{
-            println("debug: Job is not active")
-        }
-    }
-
 
     private suspend fun getResult1FromApi(): String {
         delay(1000) // Does not block thread. Just suspends the coroutine inside the thread
