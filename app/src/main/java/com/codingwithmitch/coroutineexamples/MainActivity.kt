014763c2a2247cfe6006e6619ddad02228f0cc25
Mitch Tabian <mitch@tabian.ca>
2019-07-29 10:55:08 -0700

    master

--- app/src/main/java/com/codingwithmitch/coroutineexamples/MainActivity.kt
+++ app/src/main/java/com/codingwithmitch/coroutineexamples/MainActivity.kt
@@ -35,40 +35,41 @@ class MainActivity : AppCompatActivity() {
     }
 
     private suspend fun fakeApiRequest() {
-        coroutineScope {
+        logThread("fakeApiRequest")
 
-            launch {
+        val result1 = getResult1FromApi() // wait until job is done
 
-                val result1 = getResult1FromApi() // wait until job is done
+        if ( result1.equals("Result #1")) {
 
-                if ( result1.equals("Result #1")) {
+            setTextOnMainThread("Got $result1")
 
-                    setTextOnMainThread("Got $result1")
+            val result2 = getResult2FromApi() // wait until job is done
 
-                    val result2 = getResult2FromApi() // wait until job is done
-
-                    if (result2.equals("Result #2")) {
-                        setTextOnMainThread("Got $result2")
-                    } else {
-                        setTextOnMainThread("Couldn't get Result #2")
-                    }
-                } else {
-                    setTextOnMainThread("Couldn't get Result #1")
-                }
+            if (result2.equals("Result #2")) {
+                setTextOnMainThread("Got $result2")
+            } else {
+                setTextOnMainThread("Couldn't get Result #2")
             }
-
+        } else {
+            setTextOnMainThread("Couldn't get Result #1")
         }
     }
 
 
     private suspend fun getResult1FromApi(): String {
+        logThread("getResult1FromApi")
         delay(1000) // Does not block thread. Just suspends the coroutine inside the thread
         return "Result #1"
     }
 
     private suspend fun getResult2FromApi(): String {
+        logThread("getResult2FromApi")
         delay(1000)
         return "Result #2"
+    }
+
+    private fun logThread(methodName: String){
+        println("debug: ${methodName}: ${Thread.currentThread().name}")
     }
 
 }
