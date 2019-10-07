950cf4c802109b561ba11d9307c6b2b55e750c74
Mitch Tabian <mitch@tabian.ca>
2019-10-07 09:22:49 -0700

    parallel execution with time measurement

--- app/src/main/java/com/codingwithmitch/coroutineexamples/MainActivity.kt
+++ app/src/main/java/com/codingwithmitch/coroutineexamples/MainActivity.kt
@@ -6,6 +6,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
+import kotlin.system.measureTimeMillis
 
 
 class MainActivity : AppCompatActivity() {
@@ -40,30 +41,36 @@ class MainActivity : AppCompatActivity() {
 	 * Also see "Deferred, Async, Await" branch for parallel execution
      */
     private suspend fun fakeApiRequest() {
-        coroutineScope {
+        withContext(IO) {
 
             val job1 = launch {
-                println("debug: launching job1 in thread: ${Thread.currentThread().name}")
-                val result1 = getResult1FromApi()
-                setTextOnMainThread("Got $result1")
+                val time1 = measureTimeMillis {
+                    println("debug: launching job1 in thread: ${Thread.currentThread().name}")
+                    val result1 = getResult1FromApi()
+                    setTextOnMainThread("Got $result1")
+                }
+                println("debug: compeleted job1 in $time1 ms.")
             }
 
             val job2 = launch {
-                println("debug: launching job2 in thread: ${Thread.currentThread().name}")
-                val result2 = getResult2FromApi()
-                setTextOnMainThread("Got $result2")
+                val time2 = measureTimeMillis {
+                    println("debug: launching job2 in thread: ${Thread.currentThread().name}")
+                    val result2 = getResult2FromApi()
+                    setTextOnMainThread("Got $result2")
+                }
+                println("debug: compeleted job2 in $time2 ms.")
             }
 
         }
     }
 
     private suspend fun getResult1FromApi(): String {
-        delay(1000) // Does not block thread. Just suspends the coroutine inside the thread
+        delay(1000)
         return "Result #1"
     }
 
     private suspend fun getResult2FromApi(): String {
-        delay(1000)
+        delay(1500)
         return "Result #2"
     }
 
