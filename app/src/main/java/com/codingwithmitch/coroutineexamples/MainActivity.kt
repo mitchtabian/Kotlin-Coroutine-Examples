8814f4bc82f9f5d30ac844530924629738301f31
mitch <mitch@tabian.ca>
2020-03-19 16:01:44 -0700

    basics following https://kotlinlang.org/docs/reference/coroutines/basics.html

--- app/src/main/java/com/codingwithmitch/coroutineexamples/MainActivity.kt
+++ app/src/main/java/com/codingwithmitch/coroutineexamples/MainActivity.kt
@@ -2,11 +2,6 @@ package com.codingwithmitch.coroutineexamples
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.*
-import kotlinx.coroutines.Dispatchers.IO
-import kotlinx.coroutines.Dispatchers.Main
-import kotlin.system.measureTimeMillis
 
 
 class MainActivity : AppCompatActivity() {
@@ -16,29 +11,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
     }
 
-    private fun setNewText(input: String){
-        val newText = text.text.toString() + "\n$input"
-        text.text = newText
-    }
 
-    private suspend fun setTextOnMainThread(input: String) {
-        withContext (Main) {
-            setNewText(input)
-        }
-    }
 
-    private suspend fun getResult1FromApi(): String {
-        delay(1000)
-        return "Result #1"
-    }
-
-    private suspend fun getResult2FromApi(): String {
-        delay(1700)
-        return "Result #2"
-    }
 
 }
 
