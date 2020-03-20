5864a7b93d1b5d5664e07be10ee05110a5b9e2ab
Mitch Tabian <mitch@tabian.ca>
2020-03-20 14:14:49 -0700

    setup room stuff

--- app/src/main/java/com/codingwithmitch/coroutineexamples/MainActivity.kt
+++ app/src/main/java/com/codingwithmitch/coroutineexamples/MainActivity.kt
@@ -4,11 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.*
-import kotlinx.coroutines.Dispatchers.IO
-import java.lang.Exception
-import kotlin.coroutines.CoroutineContext
-import kotlin.random.Random
 
 
 class MainActivity : AppCompatActivity() {
@@ -21,50 +16,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        main()
-
         button.setOnClickListener {
             text.setText((count++).toString())
-        }
-    }
-
-    fun main()  {
-
-        for(index in 1..10){
-            CoroutineScope(IO).launch {
-                try{
-                    println("RESULT: ${attemptConcurrentSum()}")
-                }catch (e: Exception){
-                    println("${e.message}")
-                }
-
-            }
-        }
-
-    }
-
-    suspend fun attemptConcurrentSum(): String = coroutineScope {
-        val randomNum1 = async {
-            delay(500)
-            getRandomNumber(false)
-        }
-
-        val randomNum2 = async {
-            delay(2000)
-            getRandomNumber(false)
-        }
-        val num1 = randomNum1.await()
-        val num2 = randomNum2.await()
-        println("NumOne=${num1}, NumTwo=${num2}")
-        "${(num1+ num2)}"
-    }
-
-    fun getRandomNumber(shouldFail: Boolean): Int{
-        return if(shouldFail){
-            throw ArithmeticException("Something went wrong! ")
-        }
-        else{
-            Random.nextInt(1,11)
         }
     }
 
