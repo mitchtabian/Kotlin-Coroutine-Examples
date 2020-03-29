3fa0b96513eccd017c5b58801dfff3a19fb7b54e
mitch <mitch@tabian.ca>
2020-03-29 13:22:00 -0700

    - **KEY POINTS** 1. cancel() will not cancel the parent 2. throw CancelationException() is treated like "cancel()" 3. If a coroutine encounters an exception other than CancellationException, it cancels its parent with that exception. 4. If an exception is thrown in a child coroutine, the exception is handled by the parent when all its children terminate.

--- app/src/main/java/com/codingwithmitch/coroutineexamples/MainActivity.kt
+++ app/src/main/java/com/codingwithmitch/coroutineexamples/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
-import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
 
 
 class MainActivity : AppCompatActivity() {
@@ -25,34 +25,66 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    val handler = CoroutineExceptionHandler { _, exception ->
+        println("Exception thrown in one of the children: $exception")
+    }
+
     fun main(){
-        val startTime = System.currentTimeMillis()
-        CoroutineScope(IO).launch {
-            val job1 = async(start = CoroutineStart.LAZY) { getResult1() }
-            val job2 = async(start = CoroutineStart.LAZY) { getResult2() }
-            job1.start()
-            job2.start()
-            val result1 = job1.await()
-            println("Got result1.")
-            val result2 = job2.await()
-            println("Got result2.")
-            println("Done in ${System.currentTimeMillis() - startTime} " +
-                    "ms. Result1: ${result1}, Result2: ${result2}")
+        val parentJob = CoroutineScope(Main).launch(handler) {
+
+            // --------- JOB A ---------
+            val jobA = launch {
+                val resultA = getResult(1)
+                println("resultA: ${resultA}")
+            }
+            jobA.invokeOnCompletion { throwable ->
+                if(throwable != null){
+                    println("Error getting resultA: ${throwable}")
+                }
+            }
+
+            // --------- JOB B ---------
+            val jobB = launch {
+                val resultB = getResult(2)
+                println("resultB: ${resultB}")
+            }
+            jobB.invokeOnCompletion { throwable ->
+                if(throwable != null){
+                    println("Error getting resultB: ${throwable}")
+                }
+            }
+
+            // --------- JOB C ---------
+            val jobC = launch {
+                val resultC = getResult(3)
+                println("resultC: ${resultC}")
+            }
+            jobC.invokeOnCompletion { throwable ->
+                if(throwable != null){
+                    println("Error getting resultC: ${throwable}")
+                }
+            }
+        }
+        parentJob.invokeOnCompletion { throwable ->
+            if(throwable != null){
+                println("Parent job failed: ${throwable}")
+            }
+            else{
+                println("Parent job SUCCESS")
+            }
         }
     }
 
-    suspend fun getResult1(): Int {
-        println("Starting Job1!")
-        delay(2000)
-        println("Result1 is READY...")
-        return 1
-    }
-
-    suspend fun getResult2(): Int{
-        println("Starting Job2!")
-        delay(500)
-        println("Result2 is READY...")
-        return 2
+    suspend fun getResult(number: Int): Int{
+        return withContext(Main){
+            delay(number*500L)
+            if(number == 2){
+//                cancel(CancellationException("Error getting result for number: ${number}"))
+//                throw CancellationException("Error getting result for number: ${number}") // treated like "cancel()"
+                throw Exception("Error getting result for number: ${number}")
+            }
+            number*2
+        }
     }
 
 
